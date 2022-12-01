a7e1de6969ce388e92c01aaac7d37f14eb0dc49f
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2022-12-01 13:49:17 -0700

    Mobile navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+const collapse = document.getElementById('navbar-collapse');
+const navbarLinks = document.querySelector('.navbar-links');
+
+collapse.addEventListener('click', (e) => {
+  navbarLinks.classList.toggle('show');
+});
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,13 @@
 <nav class="navbar">
-    <a href="/">Home</a>
-    <a href="#">Gallery</a>
-    <a href="#">Posts</a>
-    <a href="/about">About</a>
-    <a href="#">Contact</a>
+    <button class="navbar-collapse" id="navbar-collapse">
+        <span class="collapse-text">Collapse</span>
+    </button>
+
+    <div class="navbar-links">
+        <a href="/">Home</a>
+        <a href="#">Gallery</a>
+        <a href="#">Posts</a>
+        <a href="/about">About</a>
+        <a href="#">Contact</a>
+    </div>
 </nav>
