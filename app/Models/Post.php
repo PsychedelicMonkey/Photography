0005e07d02e9937f1b817425ab0eef96b23a9714
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-07-20 13:00:58 -0600

    Blog models

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Tags\HasTags;
+
+class Post extends Model implements HasMedia
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory, InteractsWithMedia, HasTags;
+
+    protected $guarded = [];
+
+    protected function casts():array
+    {
+        return [
+            'published_at' => 'datetime'
+        ];
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('post-images')
+            ->singleFile();
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' =>$title= $this->faker->unique()->sentence(),
+            'slug' => Str::slug($title),
+            'body' => $this->faker->realText(2000),
+            'published_at' =>$this->faker->dateTimeBetween('-1 year', '+1 month'),
+            'created_at' =>$this->faker->dateTimeBetween('-1 year', '-6 month'),
+            'updated_at' =>$this->faker->dateTimeBetween('-5 month'),
+        ];
+    }
+}
