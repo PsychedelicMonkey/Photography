22860d52730e6564dc6e99f1deaa7da8f88ad820
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-11-01 13:20:00 -0600

    Settings layout

---
+++ resources/views/layouts/settings.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-7xl mx-auto p-4 lg:p-6">
+        @if (session()->has('status'))
+            <div
+                x-data="{ show: true }"
+                x-show="show"
+                x-init="setTimeout(() => show = false, 3000)"
+                x-transition
+                role="alert"
+                class="alert alert-success"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>{{ session()->get('status') }}</span>
+            </div>
+        @endif
+
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <ul class="menu w-full">
+                <li class="menu-title">{{ __('Account settings') }}</li>
+                <li @class(['menu-active' => request()->routeIs('profile.edit')])>
+                    <a href="{{ route('profile.edit') }}">{{ __('Edit profile') }}</a>
+                </li>
+                <li>
+                    <a href="#">{{ __('Update password') }}</a>
+                </li>
+                <li>
+                    <a href="#">{{ __('Close account') }}</a>
+                </li>
+            </ul>
+
+            <div class="col-span-2">
+                @yield('form')
+            </div>
+        </div>
+
+    </div>
+@endsection
--- resources/views/settings/profile.blade.php
+++ resources/views/settings/profile.blade.php
@@ -1,34 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.settings')
 
-@section('content')
-    <div class="max-w-7xl mx-auto p-4 lg:p-6">
-        <div>
-            @if (session()->has('status'))
-                <div role="alert" class="alert alert-success">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span>{{ session()->get('status') }}</span>
-                </div>
-            @endif
-        </div>
-
+@section('form')
+    <div class="my-4">
         <h1 class="text-3xl font-semibold">{{ __('Edit your profile') }}</h1>
-
-        <form action="{{ route('profile.update') }}" method="post">
-            @csrf
-            @method('PATCH')
-            <fieldset class="fieldset w-xs p-4 border border-base-300 bg-base-200 rounded-box">
-                <label for="name" class="label">{{ __('Name') }}</label>
-                <input type="text" name="name" id="name" class="input" placeholder="{{ __('Name') }}" value="{{ old('name', $user->name) }}" />
-                <x-input-error :messages="$errors->get('name')" />
-
-                <label for="email" class="label">{{ __('Email address') }}</label>
-                <input type="email" name="email" id="email" class="input" placeholder="{{ __('Email address') }}" value="{{ old('email', $user->email) }}" />
-                <x-input-error :messages="$errors->get('email')" />
-
-                <button type="submit" class="btn btn-neutral">{{ __('Update profile') }}</button>
-            </fieldset>
-        </form>
     </div>
+
+    <form action="{{ route('profile.update') }}" method="post">
+        @csrf
+        @method('PATCH')
+        <fieldset class="fieldset w-full p-4 border border-base-300 bg-base-200 rounded-box">
+            <label for="name" class="label">{{ __('Name') }}</label>
+            <input type="text" name="name" id="name" class="input w-full" placeholder="{{ __('Name') }}" value="{{ old('name', $user->name) }}" />
+            <x-input-error :messages="$errors->get('name')" />
+
+            <label for="email" class="label">{{ __('Email address') }}</label>
+            <input type="email" name="email" id="email" class="input w-full" placeholder="{{ __('Email address') }}" value="{{ old('email', $user->email) }}" />
+            <x-input-error :messages="$errors->get('email')" />
+
+            <button type="submit" class="btn btn-neutral">{{ __('Update profile') }}</button>
+        </fieldset>
+    </form>
 @endsection
