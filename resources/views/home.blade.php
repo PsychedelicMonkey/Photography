aac4596db4da62d58e8574bf0324eecfe6709f2b
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-11-01 12:36:39 -0600

    Frontend setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-7xl mx-auto p-4 lg:p-6">
+        <h1 class="text-3xl font-semibold">{{ __('Home Page') }}</h1>
+    </div>
+@endsection
