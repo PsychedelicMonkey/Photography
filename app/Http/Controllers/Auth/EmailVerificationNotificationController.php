42b604b8c3a539e300cc064837cf8e41b16eb849
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-11-01 18:02:58 -0600

    Verify email

---
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class EmailVerificationNotificationController extends Controller
+{
+    /**
+     * Send a new email verification notification.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        /** @var User $user */
+        $user = $request->user();
+
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->intended(route('home', absolute: false));
+        }
+
+        $user->sendEmailVerificationNotification();
+
+        return back()->with('status', __('A new verification link has been sent to your email.'));
+    }
+}
--- resources/views/settings/profile.blade.php
+++ resources/views/settings/profile.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.settings')
 
 @section('form')
-    <div class="my-4">
+    <div class="my-4 flex justify-between items-baseline">
         <h1 class="text-3xl font-semibold">{{ __('Edit your profile') }}</h1>
+
+        @if ($user->hasVerifiedEmail())
+            <span class="badge badge-success">{{ __('Account confirmed') }}</span>
+        @endif
     </div>
+
+    @if (! $user->hasVerifiedEmail())
+        <div class="bg-error text-error-content p-4 rounded-box my-4 space-y-4">
+            <div>
+                <h2 class="text-lg font-bold">{{ __('Your account is not confirmed.') }}</h2>
+                <p>{{ __('You must verify your email address to upload photos or create albums.') }}</p>
+            </div>
+
+            <form action="{{ route('verification.send') }}" method="post">
+                @csrf
+
+                <button type="submit" class="btn">{{ __('Send new verification email') }}</button>
+            </form>
+        </div>
+    @endif
 
     <form action="{{ route('profile.update') }}" method="post">
         @csrf
