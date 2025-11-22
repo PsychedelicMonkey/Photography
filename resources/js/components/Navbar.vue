a349d49eb6ca4fc7e36b627c5267776cb50b8035
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-11-22 10:52:16 -0700

    DaisyUI

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+const name = page.props.name;
+</script>
+
+<template>
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="flex-1">
+            <a class="btn text-xl btn-ghost">{{ name }}</a>
+        </div>
+        <div class="flex gap-2">
+            <input
+                type="text"
+                placeholder="Search"
+                class="input-bordered input w-24 md:w-auto"
+            />
+            <div class="dropdown dropdown-end">
+                <div
+                    tabindex="0"
+                    role="button"
+                    class="btn avatar btn-circle btn-ghost"
+                >
+                    <div class="w-10 rounded-full">
+                        <img
+                            alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                        />
+                    </div>
+                </div>
+                <ul
+                    tabindex="-1"
+                    class="dropdown-content menu z-1 mt-3 w-52 menu-sm rounded-box bg-base-100 p-2 shadow"
+                >
+                    <li>
+                        <a class="justify-between">
+                            Profile
+                            <span class="badge">New</span>
+                        </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Home Page" />
 
-    <div class="mx-auto max-w-7xl p-4 lg:p-6">
-        <h1 class="text-3xl font-semibold">Home Page</h1>
-    </div>
+    <AppLayout>
+        <div class="mx-auto max-w-7xl p-4 lg:p-6">
+            <div class="mb-4">
+                <h1 class="text-3xl font-semibold">Home Page</h1>
+            </div>
+
+            <fieldset
+                class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4"
+            >
+                <label for="email" class="label">Email address</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email address"
+                    class="input"
+                />
+
+                <label for="password" class="label">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    class="input"
+                />
+
+                <label class="label">
+                    <input
+                        type="checkbox"
+                        name="remember"
+                        id="remember"
+                        class="checkbox"
+                    />
+                    Remember me
+                </label>
+
+                <button type="submit" class="btn btn-neutral">Login</button>
+            </fieldset>
+        </div>
+    </AppLayout>
 </template>
