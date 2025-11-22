e057939268021e3c0393a3d867d7e057e9c58fe7
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-11-22 10:45:40 -0700

    Global types

---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,19 @@
+export interface Auth {
+    user: User;
+}
+
+export type AppPageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+    name: string;
+    auth: Auth;
+};
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
